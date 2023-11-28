9ff0148a9bbe522c7dac2528f34e11bc47190a6d
ahmedrehane <rehaneahmed49@gmail.com>
2023-11-28 15:47:24 +0100

    monsieur propre

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Heading from "./components/heading";
+
+function App() { 
+    return (
+        <div className="App">
+            <Heading name ="ahmed" card ="rehane" />
+            <Heading name ="islam" card ="invit" />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+function Heading(props) {
+    return (
+        <h1>Hello,{props.name} - {props.card} </h1>
+    )
+}
+
+export default Heading;
